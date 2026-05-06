b39abbb257ea2c2907bc124b424ba434c0942287
Sai Kumar Erumalla <saikumar.erumalla@gmail.com>
2026-05-06 12:52:36 +0530

    Authentication added

---
+++ backend/src/main/java/com/school/sms/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.school.sms.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
